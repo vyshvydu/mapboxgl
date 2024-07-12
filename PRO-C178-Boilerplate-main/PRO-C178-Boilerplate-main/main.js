4592e4332a92807acc8a829b2d8a302184c53c5e
vyshvydu <79267593+vyshvydu@users.noreply.github.com>
2024-07-12 17:58:09 +0530

    Add files via upload

---
+++ PRO-C178-Boilerplate-main/PRO-C178-Boilerplate-main/main.js
@@ -0,0 +1,27 @@
+let latitude=22.7868542, longitude=88.3643296;
+
+// Initializing Mapbox
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXBvb3J2ZWxvdXMiLCJhIjoiY2ttZnlyMDgzMzlwNTJ4a240cmEzcG0xNyJ9.-nSyL0Gy2nifDibXJg4fTA';
+
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v11',
+	center: [longitude, latitude],
+	zoom: 16
+});   
+
+map.addControl(
+	new mapboxgl.GeolocateControl({
+		positionOptions: {
+			enableHighAccuracy: true
+		},
+		trackUserLocation: true
+	})
+);
+
+map.addControl(
+	new MapboxDirections({
+		accessToken: mapboxgl.accessToken
+	}),
+	'top-left'
+);
